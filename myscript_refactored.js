c9de677aec8be93b10c86c3bac575a82bf219897
Venelin Pendev <venelin.pendev@gmail.com>
2021-10-21 22:36:08 +0100

    implement single json for the whole list

--- myscript_refactored.js
+++ myscript_refactored.js
@@ -1,6 +1,6 @@
-function childItemMarkup(labelText, initialQuantity, id, display, checked) {
+function childItemMarkup(labelText, initialQuantity, display, checked) {
     return `
-        <li class="list_item" id="${id}"">
+        <li class="list_item">
             <span class="checkmark" onclick="tickOrUntick(this)" ${checked}>
                 <span class="circle"></span>
                 <span class="tick"></span>
@@ -15,26 +15,24 @@ function childItemMarkup(labelText, initialQuantity, id, display, checked) {
     `;
 }
 
-function parentItemMarkup(labelText, initialQuantity, id, display, checked) {
+function parentItemMarkup(labelText, initialQuantity, display, checked, id) {
     return `
-        <ul class="list">
-            <li class="list_item parent_item" id="${id}">
-                <span class="arrow" onclick="collapseOnClick(this)"></span>
-                <span class="checkmark" onclick="tickOrUntick(this)" ${checked}>
-                    <span class="circle"></span>
-                    <span class="tick"></span>
-                </span>
-                <input type="text" class="newItemInput" style="display:none;">
-                <label class="list_title_label">${labelText}</label>
-                <input type="number" value="${initialQuantity}" readonly="true" style="display:${display};" min="0" pattern="[0-9]*">
-                <input type="button" value="Edit"  onclick="editOnClick(this)">
-                <span class="close" onclick="deleteParentOnClick(this)">×</span>
-            </li>
-            <li class="list_item">
-                <input type="text" class="newItemInput" required pattern="[a-zA-Z]*">
-                <input type="button" class="insertNewButton" value="Add" onclick="startLoadingScreen(); addChildItemOnClick(this);">
-            </li>
-        </ul>
+        <li class="list_item parent_item" id="${id}">
+            <span class="arrow" onclick="collapseOnClick(this)"></span>
+            <span class="checkmark" onclick="tickOrUntick(this)" ${checked}>
+                <span class="circle"></span>
+                <span class="tick"></span>
+            </span>
+            <input type="text" class="newItemInput" style="display:none;">
+            <label class="list_title_label">${labelText}</label>
+            <input type="number" value="${initialQuantity}" readonly="true" style="display:${display};" min="0" pattern="[0-9]*">
+            <input type="button" value="Edit"  onclick="editOnClick(this)">
+            <span class="close" onclick="deleteParentOnClick(this)">×</span>
+        </li>
+        <li class="list_item">
+            <input type="text" class="newItemInput" required pattern="[a-zA-Z]*">
+            <input type="button" class="insertNewButton" value="Add" onclick="startLoadingScreen(); addChildItemOnClick(this);">
+        </li>
     `;
 }
 
@@ -42,61 +40,58 @@ function loaderMarkup() {
     return `<div class="loader"></div>`
 }
 
-class ChildItem {
-    constructor(label, quantity, id, checked) {
-        if (!id) {
-            id = currentID++;
-        }
-
-        let display = "none";
+class Item {
+    constructor(label, quantity, shouldCheck) {
+        this.label = label;
+        this.quantity = quantity;
+        this.display = "none";
         if (quantity > 0) {
-            display = "inline";
+            this.display = "inline";
         }
 
-        let thisChecked = "";
-        if (checked) {
-            thisChecked = "checked";
+        this.checked = "";
+        if (shouldCheck) {
+            this.checked = "checked";
         }
-
-        this.id = id;
-        this.html = childItemMarkup(label, quantity, id, display, thisChecked);
     }
 
     toString() {
         return this.html;
     }
+
+    isBeingEdited() {
+        if (this.label.style.display == "none") {
+            return true;
+        }
+        return false;
+    }
 }
 
+class ChildItem extends Item {
+    constructor(label, quantity, checked) {
+        super(label, quantity, checked);
+        this.html = childItemMarkup(this.label, this.quantity, this.display, this.checked);
+    }
+}
 
-class ParentItem {
-    constructor(label, quantity, id, checked) {
-        if (!id) {
-            id = currentID++;
-        }
+class ParentItem extends Item {
+    constructor(label, quantity, checked, id) {
+        super(label, quantity, checked);
 
-        let display = "none";
-        if (quantity > 0) {
-            display = "inline";
-        }
-        
-        let thisChecked = "";
-        if (checked) {
-            thisChecked = "checked";
-        }
-
-        this.id = id;
-        this.html = parentItemMarkup(label, quantity, id, display, thisChecked);
+        this.id = id
+        this.ulTag = `<ul class="list">`;
+        this.html = parentItemMarkup(this.label, this.quantity, this.display, this.checked, this.id);
+        this.ulCloseTag = `</ul>`;
     }
 
-    appendChild(childHTML) {
-        this.html += childHTML;
+    appendChild(childItem) {
+        this.html += childItem.html;
     }
 
     toString() {
-        return this.html;
+        return this.ulTag + this.html + this.ulCloseTag;
     }
 }
-
 function editOnClick(element) {
     if (!validateQuantity(element.previousElementSibling)) {
         return
@@ -105,11 +100,12 @@ function editOnClick(element) {
     const quantity = element.previousElementSibling;
     const label = quantity.previousElementSibling;
     const textBox = label.previousElementSibling;
+    const checkmark = textBox.previousElementSibling;
+    const checked = checkmark.hasAttribute("checked");
 
     if (label.style.display == "none") {
         // save new changes
         label.style.display = "inline";
-
         textBox.style.display = "none";
         label.innerHTML = textBox.value;
 
@@ -118,22 +114,34 @@ function editOnClick(element) {
         }
         quantity.setAttribute("readonly", "true");
 
-        // if the element is a parent item, both IDs will be the same
-        const parentID = element.parentNode.parentNode.children[0].id;
-        const thisID = element.parentNode.id;
-        
-        let thisType = "child";
-        if (element.parentNode.classList.contains("parent_item")) {
-            thisType = "parent";
-        }
+        // the parentID will be either the parentID for a child item
+        // or the self id for a parent item
+        const listItem = element.parentNode;
+        const parentId = parentIndex(listItem);
 
-        const updatedItem = {
-            type: thisType,
-            label: label.innerHTML,
-            quantity: quantity.value,
-            parentID: parentID,
+        let jsonList = JSON.parse(localStorage.getItem("list"));
+        // if we're in a parent item
+        let updatedItem;
+        if (element.parentNode.classList.contains("parent_item")) {
+            updatedItem = {
+                id: parentId,
+                label: label.innerHTML,
+                quantity: quantity.value,
+                checked: checked,
+                children: jsonList[parentId].children,
+            }
+            jsonList[parentId] = updatedItem;
         }
-        localStorage.setItem(thisID, JSON.stringify(updatedItem))
+        else {
+            updatedItem = {
+                parentId: parentId,
+                label: label.innerHTML,
+                quantity: quantity.value,
+                checked: checked,
+            }
+            jsonList[parentId].children[childIndex(element.parentNode)] = updatedItem;
+        }
+        localStorage.setItem("list", JSON.stringify(jsonList));
     }
     else {
         // make editable
@@ -147,27 +155,74 @@ function editOnClick(element) {
     }
 }
 
+function childIndex(element) {
+    let index = 0;
+    while (element && element.previousElementSibling) {
+        index++;
+        element = element.previousElementSibling;
+        if (element.classList && element.classList.contains("parent_item")) {
+            // subtract 2 bcs teh first 2 items from a list are 
+            // parent item and input field for the children
+            return index - 2;
+        }
+    }
+}
+
+function parentIndex(element) {
+    if (element.classList && element.classList.contains("parent_item")) {
+        return element.id;
+    }
+    while (element && element.previousElementSibling) {
+        element = element.previousElementSibling;
+        if (element.classList && element.classList.contains("parent_item")) {
+            return element.id;
+        }
+    }
+}
+
 function deleteChildOnClick(element) {
     const listItem = element.parentNode;
-    listItem.remove();
-
+    const childId = childIndex(listItem);
+    const parentId = parentIndex(listItem);
     //remove from client-side storage
-    localStorage.removeItem(listItem.id)
+    let jsonList = JSON.parse(localStorage.getItem("list"));
+    jsonList[parentId].children.splice(childId, 1);
+
+    localStorage.setItem("list", JSON.stringify(jsonList));
+    
+    listItem.remove();
 }
 
 function deleteParentOnClick(element) {
     if (element.parentNode.parentNode.children.length > 2 && !window.confirm("This item has sub-items. Are you sure you want to delete everything?")) {
         return;
     }
+    const listItem = element.parentNode; 
     const list = element.parentNode.parentNode;
-    list.remove();
+    let traversingList = list;
+    
+    //const childId = childIndex(listItem);
+    const parentId = parentIndex(listItem);
+    //remove from client-side storage
+    let jsonList = JSON.parse(localStorage.getItem("list"));
+    for (let tempParentId = parentId; tempParentId < jsonList.length; tempParentId++) {
+        parent = jsonList[tempParentId];
+        for (let child of jsonList[parent.id].children) {
+            child.parentId--;
+        }
+        traversingList.children[0].id = +traversingList.children[0].id - 1;
+        traversingList = traversingList.nextElementSibling;
+        parent.id--;
+    }
+    jsonList.splice(parentId, 1);
+    localStorage.setItem("list", JSON.stringify(jsonList));
 
     // remove from client-side storage
     // remove all children nodes first
-    for (let childIndex = 0; childIndex < list.children.length; childIndex++) {
+    /*for (let childIndex = 0; childIndex < list.children.length; childIndex++) {
         localStorage.removeItem(list.children[childIndex].id);
-    }
-    localStorage.removeItem(list.id)
+    }*/
+    list.remove();
 }
 
 function collapseOnClick(element) {
@@ -199,30 +254,31 @@ function tickOrUntick(element) {
     const label = textBox.nextElementSibling;
     const quantity = label.nextElementSibling;
 
-    // if the element is a parent item, both IDs will be the same
-    const parentID = element.parentNode.parentNode.children[0].id;
-    const thisID = element.parentNode.id;
-    
-    let thisType = "child";
-    if (element.parentNode.classList.contains("parent_item")) {
-        thisType = "parent";
-    }
+    const listItem = element.parentNode;
+    const parentId = parentIndex(listItem);
+    let jsonList = JSON.parse(localStorage.getItem("list"));
+    let updatedItem;
+        if (element.parentNode.classList.contains("parent_item")) {
+            updatedItem = {
+                id: parentId,
+                label: label.innerHTML,
+                quantity: quantity.value,
+                checked: checked,
+                children: jsonList[parentId].children,
+            }
+            jsonList[parentId] = updatedItem;
+        }
+        else {
+            updatedItem = {
+                parentId: parentId,
+                label: label.innerHTML,
+                quantity: quantity.value,
+                checked: checked,
+            }
+            jsonList[parentId].children[childIndex(element.parentNode)] = updatedItem;
+        }
 
-    const updatedItem = {
-        type: thisType,
-        label: label.innerHTML,
-        quantity: quantity.value,
-        parentID: parentID,
-        checked: checked
-    }
     localStorage.setItem(thisID, JSON.stringify(updatedItem))
-}
-
-// parentElement should be the <ul> around the child
-function addChildItemCustom(parentElement, childItemLabel, childItemQuantity, thisID, checked) {
-    setTimeout(stopLoadingScreen, 500);
-    const newChildItem = new ChildItem(childItemLabel, childItemQuantity, thisID, checked);
-    parentElement.innerHTML += newChildItem;
 }
 
 // element should be the button which the func is called from
@@ -234,27 +290,27 @@ function addChildItemOnClick(element) {
         return;
     }
 
-    // Insertion part
-    thisID = currentID++;
+    const listItem = element.parentNode;
 
-    childItemLabel = element.previousElementSibling.value;
-        childItemQuantity = 0;
-    element.previousElementSibling.value = "";
-    parentElement = element.parentNode.parentNode;
-    const thisParentID = parentElement.children[0].id;
+    const inputField = element.previousElementSibling
+    const childItemLabel = inputField.value;
+    inputField.value = "";
+    const childItemQuantity = 0;
+    let parentElement = element.parentNode.parentNode;
+    const parentId = parentIndex(listItem);
 
-    // only save the child in the storage if created from the button
-    // (otherwise, the function is called on initialisation, so
-    // item already exists in the DB)
     const newChildItemJSON = {
-        type: "child",
+        parentId: parentId,
         label: childItemLabel,
         quantity: childItemQuantity,
-        parentID: thisParentID,
     }
 
-    localStorage.setItem(thisID, JSON.stringify(newChildItemJSON));
-    const newChildItem = new ChildItem(childItemLabel, childItemQuantity, thisID);
+    jsonList = JSON.parse(localStorage.getItem("list"));
+    console.log("......" + jsonList[parentId]);
+    jsonList[parentId].children.push(newChildItemJSON);
+
+    localStorage.setItem("list", JSON.stringify(jsonList));
+    const newChildItem = new ChildItem(childItemLabel, childItemQuantity);
 
     parentElement.innerHTML += newChildItem;
 }
@@ -262,7 +318,7 @@ function addChildItemOnClick(element) {
 function addParentItem() {
     setTimeout(stopLoadingScreen, 500);
     const inputField = document.getElementsByClassName("newListInput")[0];
-    
+
     // Validation part
     if (!validateListInput(inputField)) {
         return;
@@ -272,20 +328,22 @@ function addParentItem() {
     const parentItemLabel = document.getElementsByClassName("newListInput")[0].value;
     document.getElementsByClassName("newListInput")[0].value = "";
 
-    const newParentItem = new ParentItem(parentItemLabel, 0);
-
-    document.body.innerHTML += newParentItem;
-
+    jsonList = JSON.parse(localStorage.getItem("list"));
     // This is called from the index.html only
     // so it's safe that on every click,
     // the parent item will need to be saved in local storage
     const newParentItemJSON = {
-        type: "parent",
+        id: jsonList.length,
         label: parentItemLabel,
         quantity: 0,
+        children: [],
     }
-
-    localStorage.setItem(newParentItem.id, JSON.stringify(newParentItemJSON));
+    
+    jsonList.push(newParentItemJSON);
+    localStorage.setItem("list", JSON.stringify(jsonList));
+    
+    const newParentItem = new ParentItem(parentItemLabel, 0, false, jsonList.length - 1);
+    document.body.innerHTML += newParentItem;
 }
 
 function loaderMarkup() {
@@ -363,7 +421,7 @@ function removeErrors(element) {
     while (currentElement && (!currentElement.className || currentElement.className != "error")) {
         currentElement = currentElement.nextElementSibling;
     }
-    while(currentElement && currentElement.className && currentElement.className == "error") {
+    while (currentElement && currentElement.className && currentElement.className == "error") {
         const tempElement = currentElement;
         currentElement = currentElement.nextElementSibling;
         tempElement.remove();
@@ -376,51 +434,60 @@ function removeErrors(element) {
 document.body.innerHTML += bigWhiteRectMarkup();
 document.body.innerHTML += loaderMarkup();
 
-startLoadingScreen(); 
+//startLoadingScreen(); 
 
 let currentID = 1;
 // local storage part
-const shoppingList = {
-    type: "parent",
-    label: "shopping list",
-    quantity: 10,
-    checked: false,
-};
 
-localStorage.setItem(currentID++, JSON.stringify(shoppingList));
+let jsonList = JSON.parse(localStorage.getItem('list'));
+if (!jsonList || (jsonList && jsonList.length == 0)) {
+    // prefill some data if there is no data on the site
+    const data =
+        [{
+            id: 0,
+            label: "shopping list",
+            quantity: 10,
+            checked: false,
+            children: [
+                {
+                    parentId: 0,
+                    label: "apples",
+                    quantity: 4,
+                    checked: false,
+                },
+            ],
+        },
+        {
+            id: 1,
+            label: "gas",
+            quantity: 0,
+            checked: false,
+            children: [
+                {
+                    parentId: 1,
+                    label: "diesel",
+                    quantity: 1,
+                    checked: false,
+                },
+            ],
+        }
+        ];
 
-// construct parent nodes first 
-for (const [id, value] of Object.entries(localStorage)) {
-    const args = JSON.parse(localStorage.getItem(id));
-    if (args.type == "parent") {
-        const parentItem = new ParentItem(args.label, args.quantity, id, args.checked)
-        document.body.innerHTML += parentItem;
-    }
+    localStorage.setItem("list", JSON.stringify(data));
+
+    jsonList = JSON.parse(localStorage.getItem("list"));
 }
 
-// construct children nodes 
-const apples = {
-    type: "child",
-    label: "apples",
-    quantity: 4,
-    parentID: 1,
-    checked: false,
-};
+// populate the page
+for (let parent of jsonList) {
+    //console.log(parentItem);
+    let parentItem = new ParentItem(parent.label, parent.quantity, parent.checked, parent.id);
 
-localStorage.setItem(currentID++, JSON.stringify(apples));
 
-// append children
-for (const [id, value] of Object.entries(localStorage)) {
-    // meanwhile update currentID to be the max available ID, so as not to overwrite localStorage items
-    if (id >= currentID) {
-        currentID = +id + 1;
+    for (let child of parent.children) {
+        console.log(child);
+        parentItem.appendChild(new ChildItem(child.label, child.quantity, child.checked));
     }
 
-    const args = JSON.parse(localStorage.getItem(id));
-    if (args.type == "child") {
-        const parent = findParent(args.parentID);
-        setTimeout(stopLoadingScreen, 500);
-        const newChildItem = new ChildItem(args.label, args.quantity, id, args.checked);
-        parent.parentNode.innerHTML += newChildItem;
-    }
+    document.body.innerHTML += parentItem;
 }
